822e348e0200b8f68d0dfc4c44796b64da2fa6fe
Adrian Avram <adrian.avram@talkspace.com>
2019-12-26 21:48:18 +0200

    add think api handling

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import { Provider } from 'react-redux'
 import './App.css';
 import store from './redux/store'
 // import CakeContainer from "./components/CakeContainer";
-import HooksCakeContainer from "./components/HooksCakeContainer";
-import IceCreamContainer from "./components/IceCreamContainer";
-import NewCakeContainer from "./components/NewCakeContainer";
-import ItemContainer from "./components/ItemContainer";
+// import HooksCakeContainer from "./components/HooksCakeContainer";
+// import IceCreamContainer from "./components/IceCreamContainer";
+// import NewCakeContainer from "./components/NewCakeContainer";
+// import ItemContainer from "./components/ItemContainer";
+import UserContainer from "./components/UserContainer";
 
 function App() {
   return (
       <Provider store={store}>
           <div className="App">
               {/* <CakeContainer/> */}
-              <ItemContainer/>
-              <ItemContainer cake/>
-              <HooksCakeContainer/>
-              <IceCreamContainer/>
-              <NewCakeContainer/>
+              {/*<ItemContainer/>*/}
+              {/*<ItemContainer cake/>*/}
+              {/*<HooksCakeContainer/>*/}
+              {/*<IceCreamContainer/>*/}
+              {/*<NewCakeContainer/>*/}
+              <UserContainer/>
         </div>
       </Provider>
   );
